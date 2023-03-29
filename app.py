cb39444b9c15702734232cb87084f9bd1f593a0b
Peter Giathi <pjkrash@gmail.com>
2023-03-29 17:56:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ import sys
 import instana
 
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
 
     url = os.getenv('URL')
     headers = {"content-type": "application/json; charset=utf-8" }
@@ -25,20 +21,17 @@ def hello():
         "VoterAddress":"Test Address",
         "VotingIntention": "B"
         }
+    while True:
+        print("Sending request to " + url)
+        response = requests.post(url, headers=headers, json=data)
+        print("Status Code", response.status_code)
+        print("Response: ", response.text)
+        #time.sleep(5)
 
-    print("Sending request to " + url)
-    response = requests.post(url, headers=headers, json=data)
-    print("Status Code", response.status_code)
-    print("Response: ", response.text)
-    #time.sleep(5)
+        html = "<h3>Hello {name}!</h3>" \
+                "<b>Sent request to: {url}<br/>"
+        return html.format(name=os.getenv("NAME", "APIC User"), url=os.getenv("URL","https://default.svc"))
+        return 'My hostname is %s'
+        print("JSON Response ", response.json())
 
-    html = "<h3>Hello {name}!</h3>" \
-            "<b>Sent request to: {url}<br/>"
-    return html.format(name=os.getenv("NAME", "APIC User"), url=os.getenv("URL","https://default.svc"))
-    return 'My hostname is %s'
 
-    
-    #print("JSON Response ", response.json())
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
